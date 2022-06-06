a830a2b867c0aaab2637bab1f5b649e00c26e532
rafaeldegomes <rafael.degomes@gmail.com>
2022-06-06 15:07:42 -0300

    nicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,16 @@
 
 @section('content')
 
+<div id="search-container" class="col-md-12">
+    <h1> Buscar Evento </h1>
+    <forsm action="">
+        <input type="text" id="search" name="search" class="form-control" placaholder="Buscar seu eveno">
+
+    </forsm>
+</div>
+
 @foreach($events as $event)
-    <p>{{ $event->titulo }} -- {{ $event->teste }}</p>
+    <p>{{ $event->title }} -- {{ $event->teste }}</p>
 @endforeach
 
 @endsection
